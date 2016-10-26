05209c0f3c3959b2b03865b47978d876e33cc554
Sebastian Herscher <herscher.sebastian@gmail.com>
2016-10-26 12:35:00 -0400

    rudimentary teleportation.

---
+++ Teleportation/TeleportLocation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace FRL.IO {
+  public class TeleportLocation : MonoBehaviour, IPointerTriggerPressDownHandler, IPointerStayHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public Teleporter teleporter;
+
+    public  GameObject cursorPrefab;
+
+    private Dictionary<BaseInputModule, GameObject> cursors = new Dictionary<BaseInputModule, GameObject>();
+
+
+    void IPointerTriggerPressDownHandler.OnPointerTriggerPressDown(ViveControllerModule.EventData eventData) {
+      teleporter.Teleport(eventData.worldPosition);
+    }
+
+    void IPointerStayHandler.OnPointerStay(PointerEventData eventData) {
+      GameObject cursor = cursors[eventData.module];
+      cursor.transform.position = eventData.worldPosition;
+      cursor.transform.up = eventData.worldNormal;
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData) {
+      cursors[eventData.module] = GameObject.Instantiate(cursorPrefab);
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData) {
+      GameObject.Destroy(cursors[eventData.module]);
+      cursors[eventData.module] = null;
+    }
+  }
+}
+
