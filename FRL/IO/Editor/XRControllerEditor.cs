165ce04a5e2b8169afc852b5453c06fffe96cb2e
Sebastian Herscher <herscher.sebastian@gmail.com>
2018-01-20 16:56:42 -0500

    thumbstick inversion for CV1?

--- FRL/IO/Editor/XRControllerEditor.cs
+++ FRL/IO/Editor/XRControllerEditor.cs
@@ -7,9 +7,9 @@ namespace FRL.IO {
 
     private List<InputAxis> xrAxes = new List<InputAxis>() {
       new InputAxis("LThumbstickX","","","","","","",0,0.001f,1,true,false,AxisType.JoystickAxis,1,0),
-      new InputAxis("LThumbstickY","","","","","","",0,0.001f,1,true,false,AxisType.JoystickAxis,2,0),
+      new InputAxis("LThumbstickY","","","","","","",0,0.001f,1,true,true,AxisType.JoystickAxis,2,0),
       new InputAxis("RThumbstickX","","","","","","",0,0.001f,1,true,false,AxisType.JoystickAxis,4,0),
-      new InputAxis("RThumbstickY","","","","","","",0,0.001f,1,true,false,AxisType.JoystickAxis,5,0),
+      new InputAxis("RThumbstickY","","","","","","",0,0.001f,1,true,true,AxisType.JoystickAxis,5,0),
       new InputAxis("WMR_LTouchpadX","","","","","","",0,0.001f,1,true,false,AxisType.JoystickAxis,17,0),
       new InputAxis("WMR_LTouchpadY","","","","","","",0,0.001f,1,true,false,AxisType.JoystickAxis,18,0),
       new InputAxis("WMR_RTouchpadX","","","","","","",0,0.001f,1,true,false,AxisType.JoystickAxis,19,0),
